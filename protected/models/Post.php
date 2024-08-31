e5b37bf83057635aafe070e7fe89c52f193da6b1
Arman <armandwi.pangestu7@gmail.com>
2024-08-31 20:40:46 +0700

    feat: milestone3

--- protected/models/Post.php
+++ protected/models/Post.php
@@ -184,4 +184,38 @@ class Post extends CActiveRecord
 		Comment::model()->deleteAll('post_id = ' . $this->id);
 		Tag::model()->updateFrequency($this->tags, '');
 	}
+
+	public function addComment($comment)
+	{
+		if (Yii::app()->params['commentNeedApproval']) {
+			$comment->status = Comment::STATUS_PENDING;
+		} else {
+			$comment->status = Comment::STATUS_APPROVED;
+		}
+		$comment->post_id = $this->id;
+		return $comment->save();
+	}
+
+	public function newComment($post)
+	{
+		$comment = new Comment;
+
+		if (isset($_POST['ajax']) && $_POST['ajax'] === 'comment-form') {
+			echo CActiveForm::validate($comment);
+			Yii::app()->end();
+		}
+
+		if (isset($_POST['Comment'])) {
+			$comment->attributes = $_POST['Comment'];
+			if ($post->addComment($comment)) {
+				if ($comment->status == Comment::STATUS_PENDING) {
+					Yii::app()->user()->setFlash('commentSubmitted', 'Thank you for your comment. Your comment will be posted!');
+				}
+
+				$this->refresh();
+			}
+		}
+
+		return $comment;
+	}
 }
