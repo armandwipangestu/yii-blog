cad04024f53d0e5ff74ff56cabab068d7fa1fad8
Arman <armandwi.pangestu7@gmail.com>
2024-08-31 15:46:42 +0700

    feat: milestone2

--- protected/models/Post.php
+++ protected/models/Post.php
@@ -19,6 +19,9 @@
  */
 class Post extends CActiveRecord
 {
+	const STATUS_DRAFT = 1;
+	const STATUS_PUBLISHED = 2;
+	const STATUS_ARCHIVED = 3;
 	/**
 	 * @return string the associated database table name
 	 */
@@ -35,13 +38,17 @@ class Post extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('title, content, status, author_id', 'required'),
-			array('status, create_time, update_time, author_id', 'numerical', 'integerOnly'=>true),
-			array('title', 'length', 'max'=>128),
-			array('tags', 'safe'),
+			array('title, content, status', 'required'),
+			array('title', 'length', 'max' => 128),
+			array('status', 'in', 'range' => array(1, 2, 3)),
+			array(
+				'tags', 'match', 'pattern' => '/^[\w\s,]+$/',
+				'message' => 'Tags can only contain word characters.'
+			),
+			array('tags', 'normalizeTags'),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
-			array('id, title, content, tags, status, create_time, update_time, author_id', 'safe', 'on'=>'search'),
+			array('title, status', 'safe', 'on' => 'search'),
 		);
 	}
 
@@ -53,8 +60,18 @@ class Post extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
-			'comments' => array(self::HAS_MANY, 'Comment', 'post_id'),
+			// 'comments' => array(self::HAS_MANY, 'Comment', 'post_id'),
+			// 'author' => array(self::BELONGS_TO, 'User', 'author_id'),
 			'author' => array(self::BELONGS_TO, 'User', 'author_id'),
+			'comments' => array(
+				self::HAS_MANY, 'Comment', 'post_id',
+				'condition' => 'comments.status = ' . Comment::STATUS_APPROVED,
+				'order' => 'comments.create_time DESC'
+			),
+			'commentCount' => array(
+				self::STAT, 'Comment', 'post_id',
+				'condition' => 'status = ' . Comment::STATUS_APPROVED
+			),
 		);
 	}
 
@@ -91,19 +108,19 @@ class Post extends CActiveRecord
 	{
 		// @todo Please modify the following code to remove attributes that should not be searched.
 
-		$criteria=new CDbCriteria;
+		$criteria = new CDbCriteria;
 
-		$criteria->compare('id',$this->id);
-		$criteria->compare('title',$this->title,true);
-		$criteria->compare('content',$this->content,true);
-		$criteria->compare('tags',$this->tags,true);
-		$criteria->compare('status',$this->status);
-		$criteria->compare('create_time',$this->create_time);
-		$criteria->compare('update_time',$this->update_time);
-		$criteria->compare('author_id',$this->author_id);
+		$criteria->compare('id', $this->id);
+		$criteria->compare('title', $this->title, true);
+		$criteria->compare('content', $this->content, true);
+		$criteria->compare('tags', $this->tags, true);
+		$criteria->compare('status', $this->status);
+		$criteria->compare('create_time', $this->create_time);
+		$criteria->compare('update_time', $this->update_time);
+		$criteria->compare('author_id', $this->author_id);
 
 		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
+			'criteria' => $criteria,
 		));
 	}
 
@@ -113,8 +130,58 @@ class Post extends CActiveRecord
 	 * @param string $className active record class name.
 	 * @return Post the static model class
 	 */
-	public static function model($className=__CLASS__)
+	public static function model($className = __CLASS__)
 	{
 		return parent::model($className);
+	}
+
+	public function normalizeTags($attribute, $params)
+	{
+		$this->tags = Tag::array2string(array_unique(Tag::string2array($this->tags)));
+	}
+
+	public function getUrl()
+	{
+		return Yii::app()->createUrl('post/view', array(
+			'id' => $this->id,
+			'title' => $this->title
+		));
+	}
+
+	protected function beforeSave()
+	{
+		if (parent::beforeSave()) {
+			if ($this->isNewRecord) {
+				$this->create_time = $this->update_time = time();
+				$this->author_id = Yii::app()->user->id;
+			} else {
+				$this->update_time = time();
+			}
+
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	protected function afterSave()
+	{
+		parent::afterSave();
+		Tag::model()->updateFrequency($this->_oldTags, $this->tags);
+	}
+
+	private $_oldTags;
+
+	protected function afterFind()
+	{
+		parent::afterFind();
+		$this->_oldTags = $this->tags;
+	}
+
+	protected function afterDelete()
+	{
+		parent::afterDelete();
+		Comment::model()->deleteAll('post_id = ' . $this->id);
+		Tag::model()->updateFrequency($this->tags, '');
 	}
 }
--- protected/views/post/admin.php
+++ protected/views/post/admin.php
@@ -2,14 +2,14 @@
 /* @var $this PostController */
 /* @var $model Post */
 
-$this->breadcrumbs=array(
-	'Posts'=>array('index'),
+$this->breadcrumbs = array(
+	'Posts' => array('index'),
 	'Manage',
 );
 
-$this->menu=array(
-	array('label'=>'List Post', 'url'=>array('index')),
-	array('label'=>'Create Post', 'url'=>array('create')),
+$this->menu = array(
+	array('label' => 'List Post', 'url' => array('index')),
+	array('label' => 'Create Post', 'url' => array('create')),
 );
 
 Yii::app()->clientScript->registerScript('search', "
@@ -29,34 +29,59 @@ $('.search-form form').submit(function(){
 <h1>Manage Posts</h1>
 
 <p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
+	You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
+	or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
 </p>
 
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<?php echo CHtml::link('Advanced Search', '#', array('class' => 'search-button')); ?>
 <div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
+	<?php $this->renderPartial('_search', array(
+		'model' => $model,
+	)); ?>
 </div><!-- search-form -->
 
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'post-grid',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
-	'columns'=>array(
-		'id',
-		'title',
-		'content',
-		'tags',
-		'status',
-		'create_time',
-		/*
+<!-- <?php $this->widget('zii.widgets.grid.CGridView', array(
+			'id' => 'post-grid',
+			'dataProvider' => $model->search(),
+			'filter' => $model,
+			'columns' => array(
+				'id',
+				'title',
+				'content',
+				'tags',
+				'status',
+				'create_time',
+				/*
 		'update_time',
 		'author_id',
 		*/
+				array(
+					'class' => 'CButtonColumn',
+				),
+			),
+		)); ?> -->
+
+<?php $this->widget('zii.widgets.grid.CGridView', array(
+	'dataProvider' => $model->search(),
+	'filter' => $model,
+	'columns' => array(
 		array(
-			'class'=>'CButtonColumn',
+			'name' => 'title',
+			'type' => 'raw',
+			'value' => 'CHtml::link(CHtml::encode($data->title), $data->url)'
 		),
-	),
+		array(
+			'name' => 'status',
+			'value' => 'Lookup::item("PostStatus", $data->status)',
+			'filter' => Lookup::items('PostStatus'),
+		),
+		array(
+			'name' => 'create_time',
+			'type' => 'datetime',
+			'filter' => false,
+		),
+		array(
+			'class' => 'CButtonColumn'
+		),
+	)
 )); ?>
